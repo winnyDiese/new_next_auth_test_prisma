a89a978e034e6a809c70ed98f1345bf54ef3a62e
winnyDiese <winnydiese@gmail.com>
2024-06-04 17:10:44 +0200

    Coding register page...

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -25,5 +25,10 @@ export const authOptions = {
     session:{
         strategy: "jwt"
     },
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === "developpement",
 }
+
+const handler = NextAuth(authOptions)
+export {handler as GET, handler as POST}
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
    <div className="p-20">
       <p>Hello</p>
+      <Link href="/register">Register Page</Link>
+      <Link href="/login">Login Page</Link>
    </div>
   )
 }
