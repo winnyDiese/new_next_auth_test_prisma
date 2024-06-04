67db6cb00ca9e5924f9928b031d45b37510fd2c4
winnyDiese <winnydiese@gmail.com>
2024-06-04 17:53:22 +0200

    kubiub

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -10,5 +10,12 @@ export async function POST(request){
     const {name, email, password} = body
     console.log(body)
 
-    
+    if(!name || !email || !password){
+        return new NextResponse('Missing name, email, or password',{status:400})
+    }
+
+    const exist = await prisma.user.findUnique({
+        where
+    })
+
 } 
