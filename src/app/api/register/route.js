54486132d8344866d88fd8bb9c774f0aa56d7248
winnyDiese <winnydiese@gmail.com>
2024-06-04 17:46:36 +0200

    'lhbjhbbjlj'

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,1 +1,14 @@
 
+import bcrypt from "bcrypt"
+import { PrismaClient } from "@prisma/client"
+import { NextResponse } from "next/server"
+
+const prisma = new PrismaClient()
+
+export async function POST(request){
+    const body = await request.json()
+    const {name, email, password} = body
+    console.log(body)
+
+    
+} 
